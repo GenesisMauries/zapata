fd5e08f847f21bee7e7fa642a3d81fe1fb422f90
GenesisMauries <exodo_54@hotmail.com>
2019-09-27 11:58:24 -0500

    Speakers y archivo para hacer data dinamica

---
+++ src/js/speakers.js
@@ -0,0 +1,43 @@
+let data =[
+    {
+        "name" : "Ana Zarina Palafox Méndez",
+        "semblance": "Ana Zarina, o como se refiere a sí misma, “la bruja galáctica”, es música, escritora y trovadora mexicana. También es experta en etnomusicología y Gestión Cultural. Actualmente está trabajando con la Asociación Liefde A.C. de México en el campo de la violencia de género; con varias agencias gubernamentales, trabaja en el campo como músico y trovador, siempre desde una perspectiva de género. Miembro del Eleven Music Group, KD ha lanzado numerosos cursos y talleres de improvisación, y ha publicado numerosos libros teatrales y narrativos. También ha producido programas y colaborado en radio y televisión. En 2012 fue el primer monitor de cursos en línea sobre Arrepentimiento.",
+        "photo": "https://dummyimage.com/90x90/000/ff003c",
+        "video": "",
+        "id": 1
+
+    },
+    {
+        "name" : "Espartaco Martínez Cárdenas",
+        "semblance": "Espartaco Martínez Cárdenas posee conocimientos de danzas sagradas de India Barata-Nathyamy danzas africanas con Thiare Lamine del Ballet Nacional de Senegal; Danza Butoh en México y el extranjero, con los maestros: Yuri Nagaoka, Nike Minotau, Taketeru Kudo, Yoshiito Onho y con el grupo Yan-shu. Con lo anterior, FONCA y el Gobierno de la Cuidad de Tokio le otorgaron la beca para realizar estudios y una residencia artística con Daira kudakan, una de las compañía de danza contemporánea butoh con mayor reconocimiento en el mundo.Ha impartido cursos de dance performance en EUA y Japón (Media Seven, Taneko kaishya) Actualmente y desde el 2007 es profesor residente de dance performance en la Yoshibi University of Art and Desing en Tokio, Japón; donde prepara el proyecto Alternative fashion.Ha presentado conferencias en el Congreso Internacional de Danza Teórica, convocado por la UNAM; en el Primer Congreso Nacional de Arte y Educación, convocado por la Secretaría de Cultura de la Ciudad de México.",
+        "photo": "https://dummyimage.com/90x90/000/ff003c",
+        "video": "",
+        "id": 2
+
+    },
+    {
+        "name" : "Norma Torres",
+        "semblance": "Actriz, narradora oral, escritora y productora mexicana egresada de la Licenciatura en Actuación de la Escuela Nacional de Arte Teatral; posee un Diplomado de Literatura Infantil y Juvenil por la Universidad Iberoamericana. Es narradora oral desde el 2000, disciplina en la que ha recibido diversas becas por parte de varias instituciones gubernamentales y privadas. Desde 2005 forma parte del equipo de narradores orales de Alas y Raíces, programa de la Secretaría de Cultura. Con sus espectáculos de narración oral ha realizado giras a gran parte de la República Mexicana y algunos países de América Latina. Ha sido conductora de televisión, tallerista, docente y asistente de dirección escénica formando parte de distintas compañías de teatro. Actualmente dirige la compañía “La Torre de los Cuentos”, que genera y produce espectáculos interdisciplinarios para niñas, niños y adolescentes.",
+        "photo": "https://dummyimage.com/90x90/000/ff003c",
+        "video": "",
+        "id": 3
+
+    },
+    {
+        "name" : "Iván Torres Salguero, “Van-T”",
+        "semblance": "Van-T es un artista hip-hop originario de la Ciudad de México, activo desde 1997. Su música está comprometida con el mensaje y la sustancia que caracteriza al rap serio, intimista y reflexiva socialmente en las letras, con un sonido clásico en los beats y misceláneo en los sampleos. Van-T formó parte de los crews nogara clan y mexside familia, a finales de la década de los años 90s’. Es miembro fundador del grupo de rap mexicano “Magisterio”, junto al cual trabajó en la escena hip hop nacional durante ocho años, con quienes participó en numerosos foros y festivales de México y Estados Unidos. Aparece en el compilado de 'The amber rose freestyles' del beatmaker norteamericano, Judah, como el único rapero de habla hispana. Es productor musical en la compañía Cabaret Misterio y Cabaret Loaria Kids. Promotor de lectura y Cultura Infantil en el programa nacional Alas y Raíces de la Secretaría de Cultura y tallerista de Rap en la Red de FAROs y otros centros culturales, en donde ha impartido talleres de rap para niños y jóvenes de Guadalajara. Ha grabado LP’s en solitario, con varios beatmakers y productores nacionales, así como una compilación de temas de rap para niños del espectáculo 'funky narración'.",
+        "photo": "https://dummyimage.com/90x90/000/ff003c",
+        "video": "",
+        "id": 4
+
+    },
+    {
+        "name" : "Joel Alfredo Martínez Estrada, “Danger”",
+        "semblance": "Danger es uno de los íconos del hip-hop que ha realizado Giras en Europa además de presentaciones en México, Estados Unidos y países de América Latina. Es campeón estatal y nacional de “Freestyle battles” en distintos torneos del país, incluyendo concurso 'Cervantes en Rap' del Festival Cervantino, donde ha obtenido el primer lugar en México en dos ocasiones. Su participación como productor y juez en el concurso de Nike “Rap Battle Force” Batallas de Freestyle, le ha dado el orgullo de ser único rapero en México que ha firmado para ser la imagen de la marca. Actualmente promueve su disco “Sembrando Laureles” que está en todas las plataformas musicales. Actualmente Danger es el Director y Productor de “Secretos de Sócrates”, un Festival Multidisciplinario Internacional en donde se exponen las diferentes disciplinas que tienen alguna relación con la palabra, como Rap, Poesía, Slam, Spokenword, Huapango, Jaraneros, entre otros performances, así como la impartición de diferentes Talleres y Conversatorios.",
+        "photo": "https://dummyimage.com/90x90/000/ff003c",
+        "video": "",
+        "id": 5
+
+    }
+]
+getdata(data);
---
+++ src/js/videos.js
@@ -0,0 +1,23 @@
+let spacePhoto= document.getElementById("photo")
+const getdata=(data)=>{
+    console.log(data)
+    data.forEach((photo)=>{
+        let card = 
+        // let card = ` 
+        //     <section class="col-4 mt-md-3 mt-sm-3">
+        //         <section>
+        //             <h5 class="text-center">${photo.name}</h5>
+        //             <a data-toggle="collapse" href="${photo.id}" data-target="#${photo.id}" role="button" aria-expanded="false" aria-controls="collapseExample"><img src="${photo.photo}" class="img-fluid mx-auto d-block" alt="..."></a>
+        //         </section>
+        //     </section>
+        //     <section class="row mt-3">
+        //             <section class="col-12">
+        //               <section class="collapse multi-collapse" id="${photo.id}">
+        //                 <section class="card card-body">
+        //                     Aquí la info del speaker Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+        //                 </section>
+        //               </section>
+        //             </section>`
+        spacePhoto.innerHTML += card;
+    });
+}
