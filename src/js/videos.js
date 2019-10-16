c4c65159743450c4a76127e9d4116a3a6827da82
GenesisMauries <exodo_54@hotmail.com>
2019-10-16 09:09:55 -0500

    Corrige alineacion de videos y speakers

--- src/js/videos.js
+++ src/js/videos.js
@@ -21,3 +21,24 @@ const getdata=(data)=>{
         spacePhoto.innerHTML += card;
     });
 }
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows : true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-139317831-1');
